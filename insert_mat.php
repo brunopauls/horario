7243ac545805e1d176a1ad9cf5960219cf9dfc9b
gabrielsato <gabriel_sato@hotmail.com>
2012-12-17 22:10:29 -0200

    Update insert_mat.php

--- insert_mat.php
+++ insert_mat.php
@@ -1,17 +1,10 @@
 <html>
 	<head>
 		<meta http-equiv="refresh" content="0.0001; URL=materias.php">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
-
 	<body>
 		<?php
-			$con = mysql_connect("localhost:3306","root","-");
-			if (!$con){
-				die('Could not connect: ' . mysql_error());
-			}
-
-			mysql_select_db("my_db", $con);
+			$con = require_once "config.php";
 
 			$sql="INSERT INTO Materias (Nome)
 			VALUES ('$_POST[nome]')";
