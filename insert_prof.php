afc7ef6fd76f2c478be5e9b5acf1059b5f1c3a8b
gabrielsato <gabriel_sato@hotmail.com>
2012-12-17 22:10:48 -0200

    Update insert_prof.php

--- insert_prof.php
+++ insert_prof.php
@@ -1,25 +1,18 @@
 <html>
 	<head>
 		<meta http-equiv="refresh" content="0.0001; URL=professores.php">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
-
 	<body>
 		<?php
-			$con = mysql_connect("localhost:3306","root","-");
-			if (!$con){
-				die('Could not connect: ' . mysql_error());
-			}
-
-			mysql_select_db("my_db", $con);
+			$con = require_once "config.php";
 
 			$sql="INSERT INTO Professores (Nome, Sobrenome, Materias, Dias)
-			VALUES ('$_POST[nome]','$_POST[sobrenome]', '$_POST[materias]','$_POST[dias]')";
+				VALUES ('$_POST[nome]','$_POST[sobrenome]', '$_POST[materias]','$_POST[dias]')";
 
 			echo "$_POST[nome]" . "$_POST[sobrenome]" . "$_POST[materias]" . "$_POST[dias]";
 
 			if (!mysql_query($sql,$con)){
-			  die('Error: ' . mysql_error());
+				die('Error: ' . mysql_error());
 			}
 			
 			mysql_close($con);
