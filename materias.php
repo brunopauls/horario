10790caa178c2bb39c0e26733c6697c7a09346a8
gabrielsato <gabriel_sato@hotmail.com>
2012-12-17 22:11:11 -0200

    Update materias.php

--- materias.php
+++ materias.php
@@ -1,10 +1,11 @@
 <!doctype html>
+<?php include_once("config.php"); ?>
 <html>
 	<head>
 		<title>Horario Escolar</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link type='text/css' href='css/basic.css' rel='stylesheet' media='screen'>
-		<link type='text/css' href='css/confirm.css' rel='stylesheet' media='screen'>
+		<link rel="stylesheet" type="text/css" href="css/style.css" media='screen'>
+		<link rel="stylesheet" type='text/css' href='css/basic.css' media='screen'>
+		<link rel="stylesheet" type='text/css' href='css/confirm.css' media='screen'>
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type='text/javascript' src='js/jquery.simplemodal.js'></script>
 		<script type="text/javascript" src="js/funcoes.js"></script>
@@ -14,24 +15,21 @@
 			<div id="header"></div>
 			<div id="menu">
 				<ul id="nav">
-					<a href="index.html"><li>Home</li></a>
+					<a href="index.php"><li>Home</li></a>
 					<a href="professores.php"><li>Professores</li></a>
-					<a href="turmas.html"><li>Turmas</li></a>
+			<!--		<a href="turmas.html"><li>Turmas</li></a>-->
 					<a href="materias.php"><li>Materias</li></a>
-					<a href="horarios.html"><li>Horario</li></a>
+			<!--		<a href="horarios.html"><li>Horario</li></a>-->
 				</ul>
 			</div>
 			<div id="content">
 				<div class="col-un">
+
+					<!-- Imprime a tabela das materias existentes -->
 					<?php
-						$con = mysql_connect("localhost:3306","root","-");
-						if (!$con){
-			  				die('Could not connect: ' . mysql_error());
-			  			}
+						$con = require_once "config.php";
 
-						mysql_select_db("my_db", $con);
-
-						echo '<table id="tabela">
+						echo '<table class="tabela" id="confirm-dialog">
 						<tr>
 							<th>Nome</th>
 							<th>Opcoes</th>
@@ -42,41 +40,36 @@
 						while($row = mysql_fetch_array($result)){
 							echo '<tr>';
 							echo '<td>' . $row['Nome'] . '</td>';
-							echo '<td><a href="#">Editar</a> | <a href="#" class="confirm">Excluir</a></td>';
+							echo '<td><a href="#">Editar</a> | <a href="#" class="confirm materia">Excluir</a></td>';
 							echo "</tr>";
 							
 						}
 
 						echo '</table>';
 						echo '<button id="botao" class="basic">Adicionar nova materia!</button>';
-				
 					?>
 
-						<form id="basic-modal-content" name="myForm" action="insert_mat.php" method="post">
-							<label for="nome">Nome</label><br>
-							<input type="text" id="nome" name="nome"><br><br>
-							<input type="submit" id="botaoForm" value="Adicionar">
-						</form>
-					</div>
-					<!-- preload the images -->
-					<div style='display:none'> 
-						<img src='css/x.png' alt='' />
-					</div>
+					<!-- Tabela das materias -->
+					<form id="basic-modal-content" name="myForm" action="insert_mat.php" method="post">
+						<label for="nome">Nome</label><br>
+						<input type="text" id="nome" name="nome"><br><br>
+						<input type="submit" id="botaoForm" value="Adicionar">
+					</form>
+
+					<!-- Mensagem de confirmação de exclusão -->
+					<div id="confirm">
+                        <div class="header"><span>Confirmar</span></div>
+                        <div class="message"></div>
+                        <div class="buttons">
+                            <div class="no simplemodal-close">Nao</div><div class="yes">Sim</div>
+                        </div>
+                    </div>
+
 				</div>
 			</div>
 		</div>
 	</body>
-
-	<script type="text/javascript">
-		$(".basic").click(function () {
-			$("#basic-modal-content").modal({
-				escClose: true,
-				opacity: 85
-			});
-		});
-	</script>
-
-<?
-msql_close($con);
+<?php
+	msql_close($con);
 ?>
 </html>
